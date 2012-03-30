81c3ac9d0ff179c8b02d9009afc7edb3c41610f8
shirou <shirou.faw@gmail.com>
2012-03-31 00:14:44 +0900

    create table if not exists.

--- lib/fluent/plugin/out_pghstore.rb
+++ lib/fluent/plugin/out_pghstore.rb
@@ -2,11 +2,13 @@ class Fluent::PgHStoreOutput < Fluent::BufferedOutput
   Fluent::Plugin.register_output('pghstore', self)
 
   config_param :database, :string
-  config_param :table, :string, :default => 'hstore'
+  config_param :table, :string, :default => 'fluentd_store'
   config_param :host, :string, :default => 'localhost'
   config_param :port, :integer, :default => 5432
   config_param :user, :string, :default => nil
   config_param :password, :string, :default => nil
+
+  config_param :table_option, :string, :default => nil
 
   def initialize
     super
@@ -16,7 +18,10 @@ class Fluent::PgHStoreOutput < Fluent::BufferedOutput
   def start
     super
 
-    @conn = get_connection(@dbname, @host, @port, @user, @password)
+    @conn = get_connection(@database, @host, @port, @user, @password)
+
+    create_table(@table) unless table_exists?(@table)
+
   end
 
   def shutdown
@@ -25,52 +30,73 @@ class Fluent::PgHStoreOutput < Fluent::BufferedOutput
     @conn.close
   end
   
-  # This method is called when an event is reached.
-  # Convert event to a raw string.
   def format(tag, time, record)
-    [tag, time, record].to_json + "\n"
+    [tag, time, record].to_msgpack
   end
 
-  # This method is called every flush interval. write the buffer chunk
-  # to files or databases here.
-  # 'chunk' is a buffer chunk that includes multiple formatted
-  # events. You can use 'data = chunk.read' to get all events and
-  # 'chunk.open {|io| ... }' to get IO object.
   def write(chunk)
-    data = chunk.read
-    print data
+    chunk.msgpack_each {|(tag, time_str, record)|
+      sql = generate_sql(tag, time_str, record)
+      @conn.exec(sql)
+    }
   end
 
   private
 
-  INSERT_ARGUMENT = {:collect_on_error => true}
-  BROKEN_DATA_KEY = '__broken_data'
-
-  def generate_sql()
-    
-  end
-
-  def operate(table, records)
-    begin
-      record_ids, error_records = table.insert(records, INSERT_ARGUMENT)
-      if !@ignore_invalid_record and error_records.size > 0
-        operate_invalid_records(table, error_records)
-      end
-    rescue Mongo::OperationFailure => e
-      # Probably, all records of _records_ are broken...
-      if e.error_code == 13066  # 13066 means "Message contains no documents"
-        operate_invalid_records(table, records) unless @ignore_invalid_record
+  def generate_sql(tag, time, record)
+    kv_list = []
+    record.each {|(key,value)|
+      begin
+        v = Integer(value)
+      rescue ArgumentError => e
+        kv_list.push("#{key} => \"#{value}\"")  # might be string
       else
-        raise e
+        kv_list.push("#{key} => #{value}")
       end
-    end
-    records
-  end
+    }
 
+    sql =<<"SQL"
+INSERT INTO #{@table} (tag, time, record) VALUES
+('#{tag}', '#{Time.at(time)}'::TIMESTAMP WITH TIME ZONE, '#{kv_list.join(",")}');
+SQL
+    return sql
+  end
 
   def get_connection(dbname, host, port, user, password)
-    return PG.connect(:dbname => dbname, :host => host, :port => port,
-                      :user => user, :password => password)
+    if user
+      return PG.connect(:dbname => dbname, :host => host, :port => port,
+                        :user => user, :password => password)
+    else
+      return PG.connect(:dbname => dbname, :host => host, :port => port)
+    end
+  end
+
+  def table_exists?(table)
+    sql =<<"SQL"
+SELECT COUNT(*) FROM pg_tables WHERE tablename = '#{table}';
+SQL
+    res = @conn.exec(sql)
+    if res[0]["count"] == "1"
+      return true
+    else
+      return false
+    end
+  end
+
+  def create_table(tablename)
+    sql =<<"SQL"
+CREATE TABLE #{tablename} (
+  tag TEXT,
+  time TIMESTAMP WITH TIME ZONE,
+  record HSTORE
+);
+SQL
+
+    sql += @table_option if @table_option
+
+    @conn.exec(sql)
+
+    $log.warn "#{tablename} table is not exists. created."
   end
 
 end
